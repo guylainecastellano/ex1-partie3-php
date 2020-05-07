f0c9c586742e0940ca167a14122053a5c3ab9973
guylainecastellano <guylaine.a@wanadoo.fr>
2020-05-07 14:38:50 +0200

    ex1 partie3 php

--- index.php
+++ index.php
@@ -1,32 +1,16 @@
 <?php
-$month = 'mois';
-?>
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
-<head>
-  <meta charset="utf-8"/>
-  <title>ex1 partie3 php</title>
-</head>
-<body>
-  <form action="{$_SERVER['PHP_SELF']}" method="post" name="monform" id="month">
-</form>
-  <p>
-    <?php
-//@param   Array   $month   liste des mois en fçais
-    $month = array(
-      1 =>'janvier',
-      2 => 'février',
-      3 =>'mars',
-      4 => 'avril',
-      5 => 'mai',
-      6 => 'juin',
-      7 => 'juillet',
-      8 => 'aout',
-      9 => 'septembre',
-      10 => 'octobre',
-      11 => 'novembre',
-      12 => 'decembre');
-    ?>
-  </p>
-</body>
-</html>
+$months = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'decembre');
+ //@param   Array   $months   liste des mois en fçais chiffré de 0 à 11 =>'xxx', );
+$months2 = array();   //2e version
+$months2[0] = 'janvier';
+$months2[1] = 'fevrier';
+  ?>
+    <!DOCTYPE html>
+  <html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>ex1 partie3 php</title>
+  </head>
+  <body>
+  </body>
+  </html>
